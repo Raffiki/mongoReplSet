68069d7a68074a7e68461e3a5e683f76133e1075
raffiki <raf.huys@gmail.com>
2014-04-25 14:20:44 +0200

    use monoose explicitally

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 'use strict'
 
+ /*
+  * test mongodb replication set (3 members) by shutting down primary node.
+  * One of the secondary nodes should be elected as primary.
+  *
+  * Replication set is initialized on host1. This node will start as primary.
+  * cfr. ./provisioning/mongo1.sh
+  *
+  * find a per node config in ./provisioning/mongo.conf.*
+  *
+  * ------------------------------------------------------------------------------
+  * Start vagrant
+  *
+  * Check rs.config() and rs.status() on one of the active nodes.
+  *
+  * Following script writes 60 documents to the replset, with 500ms intervals.
+  * During this 30sec period, try to shutdown mongodb process of primary node.
+  * One of the seconary nodes should take writes over.
+  * The script should progress and do a successful read of 60 documents at the end.
+  */
+
 var mongoose = require('mongoose')
   , format = require('util').format
+  , async = require('async')
 
 var host1 = '192.168.33.11'
   , host2 = '192.168.33.12'
@@ -10,39 +31,43 @@ var host1 = '192.168.33.11'
   , url = format("mongodb://%s:%s,%s:%s,%s:%s/node-mongo-examples"
   , host1, port, host2, port, host3, port)
 
-mongoose.connect(url, function(err, db) {
-  if(err) throw err
+var testSchema = mongoose.Schema({
+  a: Number
+})
 
-  db.dropDatabase(function(err, result) {
-    var collection = db.collection('test')
+var User = mongoose.model('user', testSchema)
 
-    collection.remove(function(err, collection) {
-      // Insert 3 records
-      for(var i = 0 i < 3; i++) {
-        collection.insert({'a':i})
-      }
+function save(user, cb) {
+  setTimeout(function() {
+    user.save(cb)
+  }, 500)
+}
 
+mongoose.connect(url)
 
-      collection.count(function(err, count) {
-        function(){
+var db = mongoose.connection
+db.on('error', console.log.bind(console, 'connection error:'))
+db.once('open', function cb() {
 
+  // Insert 60 records
+  var data = []
+  for(var i = 0; i < 60; i++) {
+    data.push(new User({'a':i}))
+  }
 
-        console.log("There are " + count + " records in the test collection. Here they are:")
-
-        collection.find().each(function(err, item) {
-          if(item != null) {
-            console.dir(item)
-            console.log("created at " + new Date(item._id.generationTime) + "\n")
-          }
-
-          // Null signifies end of iterator
-          if(item == null) {
-            // Destory the collection
-            collection.drop(function(err, collection) {
-              db.close()
-            })
-          }
-        })
+  async.eachSeries(data, save, function(err) {
+    if (err) throw err
+    User.find(function(err, items) {
+      items.map(function(item) {
+        if(item != null) {
+          console.dir(item)
+          console.log("created at " + new Date(item._id.generationTime) + "\n")
+        }
+      })
+      console.log("There are " + items.length() + " records in the test collection")
+      User.remove({}, function(err, collection) {
+        if (err) throw err
+        db.close()
       })
     })
   })
